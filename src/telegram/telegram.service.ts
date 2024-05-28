04dcb5ad58fa33c4be521762706c5e43090dff3a
Javiink <javihamty@gmail.com>
2024-05-28 12:35:04 +0200

    🔊 Created a backdoor to test in deployments

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Action, Ctx, Hears, Start, Update } from 'nestjs-telegraf';
+import { Action, Ctx, Hears, On, Start, Update } from 'nestjs-telegraf';
 import { Context, Markup } from 'telegraf';
 import { DishesService } from '../dishes/dishes.service';
 
@@ -29,5 +29,15 @@ export class TelegramService {
   async hearsButton1(@Ctx() ctx: Context) {
     console.log(ctx);
     await ctx.reply('button 1');
+  }
+
+  @On('message')
+  async test(@Ctx() ctx: Context) {
+    const msg = ctx.text;
+    if (msg.startsWith('#test\n')) {
+      const text = msg.split('\n').slice(1).join('\n');
+      const res = await this.dishesService.getDish(text);
+      ctx.reply(res);
+    }
   }
 }
